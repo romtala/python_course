cbf699f9f600784c405255f0ae8cdbaab08d40a7
romtala <167280938+romtala@users.noreply.github.com>
2025-08-27 15:07:21 +0530

    Create Task2.py

---
+++ Module2/Task2.py
@@ -0,0 +1,11 @@
+# personalized_greeting.py
+
+# Take user input for first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate to get full name
+full_name = first_name + " " + last_name
+
+# Print personalized greeting
+print(f"Hello, {full_name}! Welcome!")
